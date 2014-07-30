b3662e5b01b09883fa0384cdc831465c3692405f
Anton <apolkhovskiy@dataart.com>
2014-07-30 14:06:01 +0300

    added tests and singletone

---
+++ HelloPattern/Patterns/Singleton.cs
@@ -0,0 +1,23 @@
+﻿namespace HelloPattern.Patterns
+{
+    public class Singleton<T> where T : class
+    {
+        private Singleton() { }
+
+        public T Value { get; set; }
+
+        public static Singleton<T> Instance
+        {
+            get { return Nested.Instance; }
+        }
+
+        private class Nested
+        {
+            internal static readonly Singleton<T> Instance = new Singleton<T>();
+
+            static Nested()
+            {
+            }
+        }
+    }
+}
---
+++ HelloTest/SingletoneTest.cs
@@ -0,0 +1,26 @@
+﻿using HelloPattern.Patterns;
+using NUnit.Framework;
+
+namespace HelloTest
+{
+    public class SingletoneTest
+    {
+        [Test]
+        public void TestSingletone()
+        {
+            Singleton<string> test = Singleton<string>.Instance;
+            test.Value = "first";
+
+            Assert.AreEqual(test.Value, "first");
+
+            var test2 = Singleton<string>.Instance;
+
+            Assert.AreEqual(test2.Value, "first");
+
+            var test3 = Singleton<int[]>.Instance;
+
+            Assert.AreNotEqual(test3.Value, "first");
+
+        }
+    }
+}
